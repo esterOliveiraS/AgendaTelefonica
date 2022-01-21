8e266676e67a00b76cab6a8d8098640182e1ed35
estercarvalho346@gmail.com <ester.oliveira@mobile2you.com.br>
2022-01-21 19:14:06 -0300

    added features javaScript

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,10 @@
 const { environment } = require('@rails/webpacker')
+const { webpack } = require('webpack')
 
+const webpacker = require('webpack')
+environment.plugin.append('Provide', new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+}))
 module.exports = environment
